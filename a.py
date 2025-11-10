e05692f53e385ca0be766fe0c7a80f3457c401bd
piyush kumar <piyushbaghel5556@gmail.com>
2025-11-10 09:57:12 -0800

    Add files via upload

---
+++ a.py
@@ -0,0 +1,33 @@
+
+# question 1
+
+num1 = 4
+num2 = 5
+num3 = num1 + num2
+print(num3)
+
+# question 2
+a = 4
+b = 9
+c = a + b
+print(c)
+print( a - b )
+print(a * b )
+print(a / b )
+print ( a + b )
+
+# question 3
+
+a = 10
+b = 20
+print("additon: " , a+b)
+print("subtraction: " , a-b)
+print("multiplication: " , a*b)
+print("division: " , a/b)
+print("exponentiation: " , a**b)
+print("remainder: " , a%b)
+print("floor division: " , a // b)
+
+
+
+
---
+++ piyush2.py
@@ -0,0 +1,6 @@
+# project 2
+
+name = "piyush "
+last = " kumar"
+print("first name : ", name)
+print('last nmae :' , last )
